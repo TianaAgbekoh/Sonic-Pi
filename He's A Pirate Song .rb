a412897c156ad4abfd360522aa3fbbcceffe426c
TianaAgbekoh <160068570+TianaAgbekoh@users.noreply.github.com>
2024-02-14 12:40:18 -0800

    Create He's A Pirate Song .rb
    Custom song of my project, still editing...

---
+++ He's A Pirate Song .rb
@@ -0,0 +1,60 @@
+use_bpm 190
+
+
+play :D, amp: 3
+sleep 1
+play :D, amp: 3
+sleep 0.5
+play :D, amp: 3
+sleep 1
+play :D, amp: 3
+sleep 0.5
+play :D, amp: 3
+sleep 1
+play :D, amp: 3
+sleep 0.5
+
+
+play :D, amp: 3
+sleep 0.5
+play :A3, amp: 3
+sleep 0.5
+play :C4, amp: 3
+sleep 0.5
+
+
+play :D, amp: 3
+sleep 1
+play :D, amp: 3
+sleep 1
+play :D, amp: 3
+sleep 0.5
+play :E4, amp: 3
+sleep 0.5
+
+
+play :F4, amp: 3
+sleep 1
+play :F4, amp: 3
+sleep 1
+play :F4, amp: 3
+sleep 0.5
+play :G4, amp: 3
+sleep 0.5
+
+
+play :E4, amp: 3
+sleep 1
+play :E4, amp: 3
+sleep 1
+play :D4, amp: 3
+sleep 0.5
+play :C4, amp: 3
+sleep 0.5
+play :C4, amp: 3
+sleep 0.5
+play :D4, amp: 3
+sleep 1
+
+
+
