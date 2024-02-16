fa38713b51cf91a0a96831a856a3e251da83d37b
TianaAgbekoh <160068570+TianaAgbekoh@users.noreply.github.com>
2024-02-16 11:35:50 -0800

    Update He's A Pirate Song .rb
    Updated Pirates of the Caribbean THEME SONG

--- He's A Pirate Song .rb
+++ He's A Pirate Song .rb
@@ -1,18 +1,37 @@
+# He's A Pirate: Custom Song
+
 use_bpm 190
+use_synth :piano
 
+live_loop :Background_beat do
+  22.times do
+    sample :bd_haus, beat_stretch: 2
+    sleep 1
+  end
+  stop
+end
 
-play :D, amp: 3
-sleep 1
-play :D, amp: 3
-sleep 0.5
-play :D, amp: 3
-sleep 1
-play :D, amp: 3
-sleep 0.5
-play :D, amp: 3
-sleep 1
-play :D, amp: 3
-sleep 0.5
+live_loop :Background2nd_beat do
+  14.times do
+    sample :drum_bass_soft, beat_stretch: 2, amp: 2
+    sleep 1.5
+  end
+  stop
+end
+
+sleep 6
+
+define :the_notes do
+  play :D, amp: 3
+  sleep 1
+  play :D, amp: 3
+  sleep 0.5
+end
+
+#Opening
+3.times do
+  the_notes
+end
 
 
 play :D, amp: 3
@@ -55,6 +74,5 @@ play :C4, amp: 3
 sleep 0.5
 play :D4, amp: 3
 sleep 1
-
 
 
