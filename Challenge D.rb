922f97221b3acfd02578f242e26e404bb2eb7312
TianaAgbekoh <160068570+TianaAgbekoh@users.noreply.github.com>
2024-02-16 11:32:02 -0800

    Create Challenge D.rb
    Spongebob ending theme song

---
+++ Challenge D.rb
@@ -0,0 +1,134 @@
+# CHALLENGE D: Use functions to optimize the SpongeBob theme!
+
+use_bpm 136
+use_synth :piano
+
+define :measure_2 do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+define :measure_3 do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+define :measure_5 do
+  
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
+end
+
+
+live_loop:background_notes do
+  play:E3, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
+  
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  5.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+
+live_loop :Second do
+  32.times do
+    sample :loop_breakbeat, beat_stretch: 2
+    sleep 1
+  end
+  stop
+end
+
+# Measure 1
+play :r
+sleep 1
+play:E4
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+sleep 1
+
+# Measure 2
+measure_2
+
+# Measure 3
+measure_3
+
+# Measure 4
+measure_3
+
+# Measure 5
+measure_5
+
+# Measure 6
+measure_3
+
+# Measure 7
+measure_5
+
+# Measure 8
+measure_2
+
+sample :drum_cymbal_hard, amp: 3
