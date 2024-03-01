4e4fa1edf2647f227c298cf6e2774d0e21862803
TianaAgbekoh <160068570+TianaAgbekoh@users.noreply.github.com>
2024-03-01 11:26:26 -0800

    He's A Pirate Song remix .rb
    For CS

--- He's A Pirate Song .rb
+++ He's A Pirate Song .rb
@@ -2,17 +2,42 @@
 
 use_bpm 190
 use_synth :piano
+x = 0.5
+s = 1
+num = 8
+
+
+Intro = "C:/Users/tiana_agbekoh/Downloads/Pirates of the Caribbean - Dead Men Tell No Tales).wav"
+Outro = "C:/Users/tiana_agbekoh/Downloads/outro.wav"
+Pirate = "C:/Users/tiana_agbekoh/Downloads/21085535_pirate-saying-fire-in-the-hole_by_applehillstudios_preview.mp3"
+
+
+define :the_notes do
+  play :D, amp: 3
+  sleep s
+  play :D, amp: 3
+  sleep x
+end
+
+
+
+sample Intro
+
+
+sleep 8
 
 live_loop :Background_beat do
-  22.times do
+  55.times do
     sample :bd_haus, beat_stretch: 2
-    sleep 1
+    sleep s
   end
   stop
 end
 
+sleep 2
+
 live_loop :Background2nd_beat do
-  14.times do
+  42.times do
     sample :drum_bass_soft, beat_stretch: 2, amp: 2
     sleep 1.5
   end
@@ -21,12 +46,6 @@ end
 
 sleep 6
 
-define :the_notes do
-  play :D, amp: 3
-  sleep 1
-  play :D, amp: 3
-  sleep 0.5
-end
 
 #Opening
 3.times do
@@ -34,45 +53,61 @@ end
 end
 
 
-play :D, amp: 3
-sleep 0.5
-play :A3, amp: 3
-sleep 0.5
-play :C4, amp: 3
-sleep 0.5
+6.times do
+  play :D, amp: 3
+  sleep x
+  play :A3, amp: 3
+  sleep x
+  play :C4, amp: 3
+  sleep x
+  
+  
+  play :D, amp: 3
+  sleep s
+  play :D, amp: 3
+  sleep s
+  play :D, amp: 3
+  sleep x
+  play :E4, amp: 3
+  sleep x
+  
+  
+  play :F4, amp: 3
+  sleep s
+  play :F4, amp: 3
+  sleep s
+  play :F4, amp: 3
+  sleep x
+  play :G4, amp: 3
+  sleep x
+  
+  
+  play :E4, amp: 3
+  sleep s
+  play :E4, amp: 3
+  sleep s
+  play :D4, amp: 3
+  sleep x
+  play :C4, amp: 3
+  sleep x
+  play :C4, amp: 3
+  sleep x
+  play :D4, amp: 3
+  sleep s
+end
+
+sample Pirate, amp: 5
+
+sleep 4
+
+sample Outro
+
+sleep 85
+
+8.times do
+  sample :bd_boom, amp: num
+  num = num -1
+  sleep s
+end
 
 
-play :D, amp: 3
-sleep 1
-play :D, amp: 3
-sleep 1
-play :D, amp: 3
-sleep 0.5
-play :E4, amp: 3
-sleep 0.5
-
-
-play :F4, amp: 3
-sleep 1
-play :F4, amp: 3
-sleep 1
-play :F4, amp: 3
-sleep 0.5
-play :G4, amp: 3
-sleep 0.5
-
-
-play :E4, amp: 3
-sleep 1
-play :E4, amp: 3
-sleep 1
-play :D4, amp: 3
-sleep 0.5
-play :C4, amp: 3
-sleep 0.5
-play :C4, amp: 3
-sleep 0.5
-play :D4, amp: 3
-sleep 1
-
-sample :bd_boom, amp: 3
